88f951c15d9650a734ee00d3fe2c75eb7605e80c
André Luis <32471143+D3rK0nig45@users.noreply.github.com>
2025-05-18 19:54:36 -0300

    auth consertado

--- Backend/abaco_backend/settings.py
+++ Backend/abaco_backend/settings.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 from datetime import timedelta
+from pathlib import Path
 import os
 from dotenv import load_dotenv
 
@@ -7,12 +7,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
-
+ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -25,13 +24,13 @@ INSTALLED_APPS = [
     'rest_framework',
 ]
 
-REST_FRAMEWORKK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': (
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ),
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
 }
 
 SIMPLE_JWT = {
@@ -58,6 +57,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -68,18 +68,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'abaco_backend.wsgi.application'
 
-# Configurações de banco de dados
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER' : os.getenv("DB_USER"),
-        'PASSWORD' : os.getenv("DB_PASSWORD"),
-        'HOST' : os.getenv("DB_HOST"),
-        'PORT' : os.getenv("DB_PORT"),
-        
-        
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
@@ -106,9 +102,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 STATIC_URL = 'static/'
-
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
--- Backend/api/views.py
+++ Backend/api/views.py
@@ -6,7 +6,6 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 class UsuarioListView(generics.ListAPIView):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
-    permission_classes = [AllowAny]
 class UsuariosCreateView(generics.CreateAPIView):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
@@ -15,43 +14,35 @@ class UsuariosCreateView(generics.CreateAPIView):
 class UsuarioDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
-    permission_classes = [AllowAny]
 
 class ComputadorListView(generics.ListCreateAPIView):
     queryset = Computador.objects.all()
     serializer_class = ComputadorSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
 class ComputadorDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Computador.objects.all()
     serializer_class = ComputadorSerializer
-    permission_classes = [AllowAny]
 
 class EquipamentoListView(generics.ListCreateAPIView):
     queryset = Equipamento.objects.all()
     serializer_class = EquipamentoSerializer
-    permission_classes = [AllowAny]
 
 class EquipamentoDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Equipamento.objects.all()
     serializer_class = EquipamentoSerializer
-    permission_classes = [AllowAny]
 
 class ManutencoesComputadoresListCreateView(generics.ListCreateAPIView):
     queryset = Manutencoes_Computadores.objects.all()
     serializer_class = ManutencaoComputadorSerializer
-    permission_classes = [AllowAny]
 
 class ManutencoesComputadoresDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Manutencoes_Computadores.objects.all()
     serializer_class = ManutencaoComputadorSerializer
-    permission_classes = [AllowAny]
 
 class ManutencoesEquipamentosListCreateView(generics.ListCreateAPIView):
     queryset = Manutencoes_Equipamentos.objects.all()
     serializer_class = ManutencaoEquipamentoSerializer
-    permission_classes = [AllowAny]
 
 class ManutencaoEquipamentoDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Manutencoes_Equipamentos.objects.all()
     serializer_class = ManutencaoEquipamentoSerializer
-    permission_classes = [AllowAny]
