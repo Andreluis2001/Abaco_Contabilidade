ed3556eb4baa83de71a47313f69f732e1230f38f
André Luis <32471143+D3rK0nig45@users.noreply.github.com>
2025-06-08 21:34:20 -0300

    csv's

--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import (ComputadorDetailView, ComputadorListView, EquipamentoDetailView, 
-                    EquipamentoListCreateView, UsuarioDetailView, UsuarioListView, UsuariosCreateView)
+                    EquipamentoListCreateView, UsuarioDetailView, UsuarioListView, UsuariosCreateView, 
+                    ExportComputadorCSVView, ExportEquipamentoCSVView, ExportAllCSVView)
 
 urlpatterns = [
     path('usuarios/', UsuarioListView.as_view(), name='usuario-list'),
@@ -9,4 +10,7 @@ urlpatterns = [
     path('computadores/<str:pk>/', ComputadorDetailView.as_view(), name='computador-detail'),
     path('equipamentos/', EquipamentoListCreateView.as_view(), name='equipamento-list-create'),
     path('equipamentos/<str:pk>/', EquipamentoDetailView.as_view(), name='equipamento-detail'),
+    path('export/computadores/csv/', ExportComputadorCSVView.as_view(), name='export-computador-csv'),
+    path('export/equipamentos/csv/', ExportEquipamentoCSVView.as_view(), name='export-equipamento-csv'),
+    path('export/tudo/csv/', ExportAllCSVView.as_view(), name='export-all-csv'),
 ]
--- Backend/api/views.py
+++ Backend/api/views.py
@@ -2,6 +2,9 @@ from rest_framework import generics
 from .models import  Computador, Equipamento, Usuario
 from .serializers import ComputadorSerializer, EquipamentoSerializer, UsuarioSerializer
 from .filters import ComputadorFilter, EquipamentoFilter, UsuarioFilter
+from django.views import View
+from django.http import HttpResponse
+import csv
 
 class UsuarioListView(generics.ListAPIView):
     queryset = Usuario.objects.all()
@@ -32,3 +35,113 @@ class EquipamentoListCreateView(generics.ListCreateAPIView):
 class EquipamentoDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Equipamento.objects.all()
     serializer_class = EquipamentoSerializer
+
+class ExportComputadorCSVView(View):
+    def get(self, request, *args, **kwargs):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="computadores.csv"'
+
+        writer = csv.writer(response)
+        unformatted_headers = Computador._meta.get_fields()
+        headers = [field.name for field in unformatted_headers]
+        writer.writerow(headers)
+
+        computadores = Computador.objects.all()
+        for computador in computadores:
+            writer.writerow([
+                computador.numero_de_patrimonio,
+                computador.modelo,
+                computador.data_de_aquisicao,
+                computador.localizacao,
+                computador.data_da_garantia,
+                computador.modelo_processador,
+                computador.memoria_ram,
+                computador.modelo_hd,
+                computador.modelo_ssd,
+                computador.modelo_fonte,
+                computador.modelo_placa_mae,
+                computador.modelo_placa_video,
+                computador.descricao,
+            ])
+
+        return response
+    
+class ExportEquipamentoCSVView(View):
+    def get(self, request, *args, **kwargs):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="equipamentos.csv"'
+
+        writer = csv.writer(response)
+        unformatted_headers = Equipamento._meta.get_fields()
+        headers = [field.name for field in unformatted_headers]
+        writer.writerow(headers)
+
+        equipamentos = Equipamento.objects.all()
+        for equipamento in equipamentos:
+            writer.writerow([
+                equipamento.numero_de_patrimonio,
+                equipamento.equipamento,
+                equipamento.modelo,
+                equipamento.data_de_aquisicao,
+                equipamento.data_da_garantia,
+                equipamento.localizacao,
+                equipamento.descricao,
+            ])
+
+        return response
+class ExportAllCSVView(View):
+    def get(self, request, *args, **kwargs):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="all_data.csv"'
+
+        writer = csv.writer(response)
+        
+        computador_headers = Computador._meta.get_fields()
+
+        headers = [
+            field.name for field in computador_headers
+        ]
+
+        headers = ['Tipo'] + headers
+
+        writer.writerow(headers)
+
+        computadores = Computador.objects.all()
+        for computador in computadores:
+            writer.writerow([
+                'Computador',
+                computador.numero_de_patrimonio,
+                computador.modelo,
+                computador.data_de_aquisicao,
+                computador.localizacao,
+                computador.data_da_garantia,
+                computador.modelo_processador,
+                computador.memoria_ram,
+                computador.modelo_hd,
+                computador.modelo_ssd,
+                computador.modelo_fonte,
+                computador.modelo_placa_mae,
+                computador.modelo_placa_video,
+                computador.descricao
+            ])
+
+        equipamentos = Equipamento.objects.all()
+        for equipamento in equipamentos:
+            writer.writerow([
+                'Equipamento',
+                equipamento.numero_de_patrimonio,
+                equipamento.equipamento,
+                equipamento.data_de_aquisicao,
+                equipamento.localizacao,
+                equipamento.data_da_garantia,
+                '',  # Modelo Processador
+                '',  # Memoria RAM
+                '',  # Modelo HD
+                '',  # Modelo SSD
+                '',  # Modelo Fonte
+                '',  # Modelo Placa Mãe
+                '',  # Modelo Placa de Vídeo
+                equipamento.descricao
+            ])
+
+        return response
